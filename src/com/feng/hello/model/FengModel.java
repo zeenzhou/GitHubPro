211f693fb69a0a42cbc7a18d80e2d3aa4436dede
zhouliang <zeenzhou@126.com>
2016-12-18 01:39:44 +0800

    commit  model add feng model

---
+++ src/com/feng/hello/model/FengModel.java
@@ -0,0 +1,40 @@
+package com.feng.hello.model;
+
+/**
+ * Created by feng on 2016/12/11.
+ */
+public class FengModel {
+    private String name;
+    private Integer age;
+    private Integer sexy;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getSexy() {
+        return sexy;
+    }
+
+    public void setSexy(Integer sexy) {
+        this.sexy = sexy;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+        FengModel fm = new FengModel();
+        System.out.println(fm);
+    }
+}
